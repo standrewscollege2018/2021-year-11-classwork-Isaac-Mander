fc6ada6a42ea714d2c3efe5324103472e7e85dd9
Isaac-Mander <ima6796@stacmail.net>
2021-02-01 09:36:54 +1300

    Hi Name

---
+++ Hi Name.py
@@ -0,0 +1,8 @@
+#Asks user name
+#Says HI and YOUR NAME
+
+print("Hi " + input("What is your name?: "))
+date = input("What is your Age?: ")
+print("Your DOB is " + str(2021 - int(date)))
+
+#print("Your expected date of death is " + str(dod))
---
+++ Variables.py
@@ -0,0 +1,7 @@
+#Print Hello MY NAME
+name = "Isaac"
+print("Hello " + name)
+
+#Intiger Var
+num1 = 16
+print(num1%15)

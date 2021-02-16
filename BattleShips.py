2fa21ce0e5856dd45e2715d1a3a6f2fac96cebdd
Isaac-Mander <ima6796@stacmail.net>
2021-02-16 14:43:08 +1300

    Update BattleShips.py

--- BattleShips.py
+++ BattleShips.py
@@ -1,3 +1,4 @@
+head = [1,2,3,4,5,6,7,8,9]
 a = [0,0,0,0,0,0,0,0,0]
 b = [0,0,0,0,0,0,0,0,0]
 c = [0,0,0,0,0,0,0,0,0]
@@ -10,20 +11,25 @@ i = [0,0,0,0,0,0,0,0,0]
 
 
 def display_grid(a,b,c,d,e,f,g,h,i):
-    print(a)
-    print(b)
-    print(c)
-    print(d)
-    print(e)
-    print(f)
-    print(g)
-    print(h)
-    print(i)
+    print("A " + str(a))
+    print("B " + str(b))
+    print("C " + str(c))
+    print("D " + str(d))
+    print("E " + str(e))
+    print("F " + str(f))
+    print("G " + str(g))
+    print("H " + str(h))
+    print("I " + str(i))
 
-display_grid(a,b,c,d,e,f,g,h,i)
 shoot = input("Input Location e.g. (A1): ")
-print(shoot[0])
-print(shoot[1])
 
 if(shoot[0] == "a"): a[int(shoot[1])-1] = 1
-display_grid(a,b,c,d,e,f,g,h,i)
+if(shoot[0] == "b"): b[int(shoot[1])-1] = 1
+if(shoot[0] == "c"): c[int(shoot[1])-1] = 1
+if(shoot[0] == "d"): d[int(shoot[1])-1] = 1
+if(shoot[0] == "e"): e[int(shoot[1])-1] = 1
+if(shoot[0] == "f"): f[int(shoot[1])-1] = 1
+if(shoot[0] == "g"): g[int(shoot[1])-1] = 1
+if(shoot[0] == "h"): h[int(shoot[1])-1] = 1
+if(shoot[0] == "i"): i[int(shoot[1])-1] = 1
+display_grid(a,b,c,d,e,f,g,h,i) 
